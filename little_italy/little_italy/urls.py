aa2fb93cd84e2212d558ce948045552e9c3dadf7
Nacho <isanccal@myuax.com>
2025-01-11 15:39:04 +0100

    mejorando la estructura

--- little_italy/little_italy/urls.py
+++ little_italy/little_italy/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from little_italy.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.home, name='home'),
+    path('', home, name='home'),
 ]
--- little_italy/little_italy/views.py
+++ little_italy/little_italy/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+import requests
 
 def home(request):
-    return render(request, 'home.html')
+    api_url = "https://api.pizzas.com/v1/pizzas"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        pizzas = response.json()
+    else:
+        api_data = {"error": "No se pudo conectar con la API."}
+
+    return render(request, "little_italy/home.html", {"api_data": api_data})
+
+    
